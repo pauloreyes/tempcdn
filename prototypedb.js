959365101c1e14f8b8bcac3581acb316cae7a083
pauloreyes <aijin02@gmail.com>
2023-06-03 01:42:12 +0800

    Update prototypedb.js

--- prototypedb.js
+++ prototypedb.js
@@ -1,33 +1,33 @@
 var database = {
-	'R1': {
+	'R001': {
 		name: "Alice Smith",
     	address: "1234 Elm Street, Springfield, IL 62704",
     	phone: "555-555-1111",
    		email: "alice.smith@example.com",
    		recipient: "Bob Johnson"
 	},
-	'R2': {
+	'R002': {
 		name: "John Doe",
     	address: "5678 Oak Avenue, Chicago, IL 60601",
     	phone: "555-555-3333",
     	email: "john.doe@example.com",
     	recipient: "Jane Brown"
 	},
-	'R3': {
+	'R003': {
 		name: "Emily Williams",
     	address: "7890 Maple Drive, New York, NY 10001",
     	phone: "555-555-5555",
     	email: "emily.williams@example.com",
     	recipient: "David Wilson"
 	},
-	'R4': {
+	'R004': {
 		name: "Michael Chen",
     	address: "4321 Cedar Road, San Francisco, CA 94101",
     	phone: "555-555-7777",
     	email: "michael.chen@example.com",
     	recipient: "Sophia Lee"
 	},
-	'R5': {
+	'R005': {
 		name: "Olivia Martinez",
     	address: "8765 Birch Lane, Los Angeles, CA 90001",
     	phone: "555-555-9999",
