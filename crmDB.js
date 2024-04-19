ea7ba48f860f0311a9d3ef5ecba56efc90f5d603
pauloreyes <aijin02@gmail.com>
2024-04-19 22:11:50 +0800

    Create crmDB.js
    New commit

---
+++ crmDB.js
@@ -0,0 +1,29 @@
+let dataDB = {
+  AGx092019: {
+    CustomerInfo: {
+      fName: 'Sarah',
+      lName: 'Smith',
+      mName: 'N/A',
+      phoneNumber: '910-999-0202',
+      email: 'sarah.smith@gmail.com',
+      idPresented: "Driver's License",
+      idNumber: '1029271619',
+    },
+    ReservationDetails: {
+      bookingSource: 'Agora',
+      roomPreference: 'Near the elevator',
+      checkInDate: '2024-10-04',
+      checkOutDate: '2024-10-06',
+      checkInTime: '14:00',
+      checkOutTime: '12:00',
+    },
+    roomDetails: {
+      roomNo: '412',
+      roomType: 'deluxe',
+      roomCharge: 100.0,
+      luxTax: 0.12,
+      serviceTax: 0.1,
+      advancePayment: 100.0,
+    },
+  },
+};
